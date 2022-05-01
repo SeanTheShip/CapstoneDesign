efadb06e71d68298b688502eebe6b9ef067726af
Sean Jung <seanjung1129@gmail.com>
2022-05-01 14:30:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="background"> 
+    
+    <div class="black-bg" v-if="modalClick==true" @click="modalClick=false">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>내용</p>
+      </div>
+    
+    </div>
+    
+    <div class="navigationbar">
+      <a v-for="(nb,i) in navigation" :key="i" @click="modalClick=true">{{navigation[i]}}</a>
+    </div>
+  
+    <div class ="big-box">
+      <thebar/>
+      <div class="mini-box">
+        <div v-for="(lb,i) in listbox" :key="i">
+          <div class="list-box">
+            <h6>
+              {{listbox[i].id}} | {{listbox[i].types}} | {{listbox[i].title}} | {{listbox[i].sport}} | {{listbox[i].region}} | {{listbox[i].date}} | 조회수 : {{listbox[i].visit}} | 댓글수 : {{listbox[i].comment}} 
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import list from './assets/list.js';
+import Thebar from './components/Commbar.vue';
+
+export default {
+  name: 'App',
+  
+  data(){
+    return{
+      navigation : ['Home', 'Community', 'Gym Rent', 'Matching'],
+      listbox : list,
+      dis : 20,
+      modalClick : false,
+    }
+  },
+
+  components: {
+    Thebar,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  margin : 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.background {
+  background: #eee;
+  height: 100vh;
+  overflow: auto;
+}
+
+.navigationbar {
+  background : rgb(20, 20, 207);
+  padding: 1.5%;
+  border : 1px solid;
+  border-radius: 10px;
+  /*margin-right: 5%;*/
+  overflow: auto;
+}
+
+.navigationbar a { 
+  box-sizing: border-box;
+  border : 1px solid;
+  border-radius: 5px;
+  color:  white;
+  padding: 1%;
+  margin : 1%;
+  /* transition : all 0.5s; */
+}
+.navigationbar a:hover{
+  background : blue;
+}
+
+.list-box {
+  border: 1px solid black;
+  background: white;
+  border-radius: 20px;
+  margin: 2%;
+}
+
+h6 {
+  margin-left: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  text-align: left;
+  width: 90%;
+}
+
+.mini-box {
+  box-sizing: border-box;
+  background : ivory;
+  margin-top : 5%;
+  width : 90%;
+  padding : auto;
+  border-radius: 5px;
+  margin-left: 5%;
+  border : 1px solid;
+  overflow: hidden;
+  float:left
+}
+
+.big-box {
+  box-sizing: border-box;
+  background: rgb(231, 231, 214);
+  margin-top: 5%;
+  width : 90%;
+  height: 80vh;
+  padding : 10px;
+  margin-left : 5%;
+  border : 1px solid;
+  overflow: hidden;
+}
+
+.black-bg{
+  width : 100%; height : 100%;
+  background : rgba(0,0,0,0.5);
+  position : fixed; padding : 20px;
+}
+
+.white-bg{
+  width : 100%; background : white;
+  border-radius : 8px;
+  padding : 1%;
+}
+
+</style>
---
+++ src/components/Commbar.vue
@@ -0,0 +1,96 @@
+<template>
+    <nav>
+        <Dropdown title="유형" :items="Types"/>
+        <Dropdown title="필터" :items="Filter"/>
+        <Dropdown title="정렬" :items="Sort" />
+        <div class="menu-item"><a href="#">item</a></div>
+    </nav>
+</template>
+
+<script>
+import Dropdown from './dropdown.vue';
+
+export default {
+    
+    name: 'the-commbar',
+    components: { Dropdown },
+    data () {
+        return {
+            Types: [
+                {
+                    title: '일반',
+                    link: '#'
+                },
+                {
+                    title: '정보',
+                    link: '#'
+                },
+                {
+                    title: '매칭',
+                    link: '#'
+                },
+                {
+                    title: '리뷰',
+                    link: '#'
+                }
+            ],
+            Filter: [
+                {
+                    title: '종목',
+                    //<Dropdown title="종목" :items="sport"/>,
+                    link: '#'
+                },
+                {
+                    title: '지역',
+                    //<Dropdown title="지역" :items="region"/>,
+                    link: '#'
+                }
+            ],
+            Sort: [
+                {
+                    title: '인기순',
+                    link: '#'
+                },
+                {
+                    title: '등록순',
+                    link: '#'
+                },
+                {
+                    title: '댓글순',
+                    link: '#'
+                }
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+    nav {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: rgb(116, 112, 112);
+    }
+
+    nav .menu-item {
+        color: white;
+        padding: 10px 20px;
+        position: relative;
+        text-align: center;
+        border-bottom: 3px solid transparent;
+        display: flex;
+        transition: 0.4s;
+    }
+
+    nav .menu-item.active,
+    nav .menu-item:hover {
+        background-color: #444;
+        border-bottom-color: red;
+    }
+
+    nav .menu-item a {
+        color: inherit;
+        text-decoration: none;
+    }
+</style>
